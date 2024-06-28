5524042e41697487748ee11216003c3ec2a9248d
Ahmet Eren Üçüncü <032290007@ogr.uludag.edu.tr>
2024-06-28 16:33:01 +0300

    mergeAlternately
    verilen iki stringi önce kısa stringten başlanacak şekilde karma algoritması

---
+++ mergeAlternately/Program.cs
@@ -0,0 +1,35 @@
+﻿public class Solution
+{
+    public string MergeAlternately(string word1, string word2)
+    {
+        string yeniString = null;
+
+
+        if (word1.Length > word2.Length)
+        {
+            for (int i = 0; i < word2.Length; i++)
+            {
+                ; yeniString += word1[i].ToString() + word2[i].ToString();
+            }
+            yeniString += word1.Substring(word2.Length);
+        }
+
+        else if (word1.Length < word2.Length)
+        {
+            for (int i = 0; i < word1.Length; i++)
+            {
+                yeniString += word1[i].ToString() + word2[i].ToString();
+            }
+            yeniString += word2.Substring(word1.Length);
+        }
+        else
+        {
+            for (int i = 0; i < word1.Length; i++)
+            {
+                yeniString += word1[i].ToString() + word2[i].ToString();
+            }
+        }
+
+        return yeniString;
+    }
+}
