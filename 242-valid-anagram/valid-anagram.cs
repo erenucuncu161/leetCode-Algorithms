961b665d0f73222e21c368004f2687a6dfc4392c
Ahmet Eren Üçüncü <114432894+erenucuncu161@users.noreply.github.com>
2024-07-03 14:37:48 +0300

    Time: 1365 ms (5.03%) | Memory: 51.3 MB (5.28%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.cs
@@ -0,0 +1,31 @@
+public class Solution
+{
+    public bool IsAnagram(string s, string t)
+    {
+        char[] dizi=t.ToCharArray();
+        if(t.Length==s.Length){
+        for(int i = 0; i < t.Length; i++)
+        {
+            
+           s= elemanDegistir(s,dizi[i],'0');
+        }
+        for(int a = 0; a < s.Length; a++)
+        {
+            if (s[a] != '0')
+            {
+                return false;
+            }
+        }
+        return true;}
+        return false;
+        
+
+    }
+    public string elemanDegistir(string kaynak,char bul,char degis){
+        int sub=kaynak.IndexOf(bul);
+        if(sub<0){
+            return kaynak;
+        }
+        return(kaynak.Substring(0,sub)+degis+kaynak.Substring(sub+1));
+    }
+}
