c01c663d8b764db262c02c862460ee80da9cfc48
Ahmet Eren Üçüncü <114432894+erenucuncu161@users.noreply.github.com>
2024-07-26 15:33:16 +0300

    Time: 31 ms (70.06%) | Memory: 11.6 MB (38.76%) - LeetSync

---
+++ 1791-richest-customer-wealth/richest-customer-wealth.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def maximumWealth(self, accounts):
+        enbuyuk=0
+        for i in range(len(accounts)):
+            sum1=0
+            for j in range(len(accounts[i])):
+                sum1+=accounts[i][j]
+                if enbuyuk<sum1:
+                    enbuyuk=sum1
+
+
+
+
+
+        
+        
+        
+        
+        
+                  
+        
+
+
+        
+        return enbuyuk
+            
+                
