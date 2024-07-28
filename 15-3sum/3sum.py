e0bf1c35534340a738f2a58acd54d00eb359417d
Ahmet Eren Üçüncü <114432894+erenucuncu161@users.noreply.github.com>
2024-07-28 18:32:24 +0300

    Time: 866 ms (59.16%) | Memory: 14.9 MB (82.41%) - LeetSync

---
+++ 15-3sum/3sum.py
@@ -0,0 +1,39 @@
+class Solution(object):
+    def threeSum(self,nums):
+        nums.sort()  # İlk olarak diziyi sıralıyoruz
+        dizi = []
+    
+        for i in range(len(nums) - 2):
+            if i > 0 and nums[i] == nums[i - 1]:  # Aynı elemandan kaçınmak için kontrol
+                continue
+            
+            left, right = i + 1, len(nums) - 1
+            while left < right:
+                toplam = nums[i] + nums[left] + nums[right]
+                if toplam == 0:
+                    dizi.append([nums[i], nums[left], nums[right]])
+                    while left < right and nums[left] == nums[left + 1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right - 1]:
+                        right -= 1
+                    left += 1
+                    right -= 1
+                elif toplam < 0:
+                    left += 1
+                else:
+                    right -= 1
+            
+        return dizi
+    
+                       
+        
+
+                        
+                
+
+
+
+
+
+
+        
